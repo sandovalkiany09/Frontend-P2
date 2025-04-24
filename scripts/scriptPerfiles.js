dce61c9002f4d0ae5b0979df6bf7858b3b3be57a
Kiany <sandovalkiany@gmail.com>
2025-04-24 10:08:25 -0600

    Actualizo URL de scriptPerfiles con graphql

--- scripts/scriptPerfiles.js
+++ scripts/scriptPerfiles.js
@@ -121,7 +121,7 @@ document.addEventListener("DOMContentLoaded", async function () {
             `;
         
             try {
-                const response = await fetch("http://localhost:3000/graphql", {
+                const response = await fetch("http://localhost:4000/graphql", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
@@ -313,7 +313,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         `;
     
         try {
-            const response = await fetch("http://localhost:3000/graphql", {
+            const response = await fetch("http://localhost:4000/graphql", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
